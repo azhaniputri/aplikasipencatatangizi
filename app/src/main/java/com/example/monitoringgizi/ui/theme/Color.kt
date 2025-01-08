94c7689643c9da05277621f0f87f84dbf4e42c6e
azhaniputri <azhaniwp@gmail.com>
2025-01-08 17:23:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/monitoringgizi/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.monitoringgizi.ui.theme
+// Warna gelap dan dinamis yang digunakan untuk tampilan aplikasi
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(0xFF6200EA)  // Ungu cerah untuk aksen utama
+val SecondaryColor = Color(0xFF03DAC6)  // Biru toska cerah untuk aksen sekunder
+val BackgroundColor = Color(0xFF121212)  // Warna latar belakang gelap
+val SurfaceColor = Color(0xFF1F1F1F)  // Warna surface gelap
+
+val OnPrimaryColor = Color.White  // Teks pada primary
+val OnSecondaryColor = Color.Black  // Teks pada secondary
+val OnBackgroundColor = Color.White  // Teks pada background
+val OnSurfaceColor = Color.White  // Teks pada surface
